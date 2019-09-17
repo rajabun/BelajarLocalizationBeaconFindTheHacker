ff115086a9ecf706ba99ea064320e9de72474d08
Muhammad Rajab <ichijo_rajab@hotmail.com>
2019-09-17 19:24:21 +0700

    Berhasil deteksi beacon punya akademi dengan UUID : CB10023F-A318-3394-4199-A8730C7C1AEC dan identifier Hacker

--- BelajarLocalizationBeaconFindTheHacker/ViewController.swift
+++ BelajarLocalizationBeaconFindTheHacker/ViewController.swift
@@ -7,14 +7,88 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, CLLocationManagerDelegate
+{
+    //bikin objek untuk location manager
+    var locationManager: CLLocationManager?
+    
+    @IBOutlet weak var distanceReading: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        //setPermission()
         // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
+    {
+        if status == .authorizedAlways
+        {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self)
+            {
+                if CLLocationManager.isRangingAvailable()
+                {
+                    // do stuff
+                    startScanning()
+                }
+            }
+        }
+    }
+    func startScanning()
+    {
+        let uuid = UUID(uuidString: "CB10023F-A318-3394-4199-A8730C7C1AEC")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 222, minor: 155, identifier: "Hacker")
+        
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func update(distance: CLProximity)
+    {
+        UIView.animate(withDuration: 0.8)
+        {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                self.distanceReading.text = "UNKNOWN"
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                self.distanceReading.text = "FAR"
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                self.distanceReading.text = "NEAR"
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+                self.distanceReading.text = "RIGHT HERE"
+            
+            @unknown default:
+                self.view.backgroundColor = .black
+                self.distanceReading.text = "WHOA!"
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion)
+    {
+        if let beacon = beacons.first
+        {
+            update(distance: beacon.proximity)
+        }
+        else
+        {
+            update(distance: .unknown)
+        }
     }
 
-
 }
-
